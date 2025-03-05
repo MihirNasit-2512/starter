97c8b4032949b8bc816f14abb175ea7303515dfd
tp <tp0814689@gmail.com>
2025-03-05 13:06:59 +0530

    Added hooks

--- model/post.js
+++ model/post.js
@@ -52,3 +52,36 @@ postSchema.pre(["find", "findOne"], function () {
 });
 
 module.exports = mongoose.models.Posts || mongoose.model("Posts", postSchema);
+
+
+// Middleware to auto-calculate points before saving
+// categoriesProgressSchema.pre("save", function (next) {
+//   this.points = this.categories.reduce(
+//     (sum, category) => sum + (category.points || 0),
+//     0
+//   );
+//   next();
+// });
+
+
+// Middleware to auto-calculate points when categories are pushed (on update)
+// categoriesProgressSchema.pre("findOneAndUpdate", async function (next) {
+//   const update = this.getUpdate();
+
+//   // If categories are being pushed
+//   if (update?.["$push"] && update?.["$push"]?.categories) {
+//     // Get the existing document before update
+//     const doc = await this.model.findOne(this.getQuery());
+
+//     if (doc) {
+//       // Calculate new points
+//       const newCategory = update?.["$push"]?.categories;
+//       const newTotalPoints = doc.points + (newCategory.points || 0);
+
+//       // Set the new points value
+//       this.set({ points: newTotalPoints });
+//     }
+//   }
+
+//   next();
+// });
